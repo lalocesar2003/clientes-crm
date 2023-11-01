c0c6fd7bd00346909d47eb2bfff7cd10c41dbf36
cesar eduardo <ceed1000@gmail.com>
2023-10-31 20:17:11 -0500

    first commit

---
+++ crm-react/src/components/Error.jsx
@@ -0,0 +1,10 @@
+import React, { Children } from 'react'
+
+function Error({children}) {
+  return (
+    <div className='text-center my-4 bg-red-600 text-white font-bold p-3 uppercase'>{Children}
+    </div>
+  )
+}
+
+export default Error
---
+++ crm-react/src/pages/Index.jsx
@@ -0,0 +1,49 @@
+import { useLoaderData } from "react-router-dom"
+import {obtenerClientes} from '../data/Clientes'
+import Cliente from "../components/Cliente";
+export function loader(){
+       
+   const clientes = obtenerClientes()
+
+    return clientes
+}
+
+function Index() {
+const clientes =useLoaderData();
+
+
+
+    return(
+<>
+<h1 className="font-black text-4xl text-blue-900">Cliente</h1>
+<p className="mt-3">administra tus clientes</p>
+{clientes.length ? (
+    <table className="w-full bg-white shadow mt-5 table-auto">
+        <thead className="bg-blue-800 text-white" >
+                <tr>
+                    <th className="p-2">Clientes</th>
+                    <th className="p-2">contacto</th>
+                    <th className="p-2">cliente</th>
+
+                </tr>
+    </thead>
+<tbody>
+    {clientes.map(cliente=>(
+<Cliente
+cliente={cliente}
+key={cliente.id}
+/>
+        
+    ))}
+</tbody>
+
+    </table>
+):(
+    <p className="text-center mt-10">no hay clientes aun</p>
+)}
+
+</>
+
+)
+}
+export default Index
